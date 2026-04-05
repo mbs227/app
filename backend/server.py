f295b38d1cdc6254483757e3dc6b39caed865681
mbs227 <msmbs_@outlook.com>
2026-04-05 15:50:19 -0400

    Restrict CORS to specific origin

--- backend/server.py
+++ backend/server.py
@@ -740,7 +740,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["https://app-seven-mu-88.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
